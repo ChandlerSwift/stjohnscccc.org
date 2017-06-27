a3edca9ad6a7badec30037051403024ba5470341
Chandler Swift <chandler@chandlerswift.com>
2017-06-27 01:59:52 +0000

    Added view files

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+@section('content')
+            <div class="right_col_section">
+              <h1>History of St. John's</h1>
+              <p><a href="images/st_johns_old.jpg"><img src="images/st_johns_old_sm.jpg" alt="church" /></a>The congregation was organized in 1865, making us one of the oldest existing congregations in McLeod County.  The 17 originating members were of German heritage. The early settlers first held services at the Philip Block home and then in the school near Biscay.</p>
+              <p>In the late 1860&#x2019;s the 40-acre site where the church now stands was purchased, and the first church was built. It burned down about 1890.</p>
+              <p>The second church building was constructed in 1883. A tornado destroyed the second church building along with the barn and schoolhouse in 1904.</p>
+              <p>Our present church building was erected in 1904. The basement was added in 1950, and the narthex in 1976.</p>
+              <p>All the original services were done in German with the official language changing to English gradually during the mid-late 1920&#x2019;s and into the 30&#x2019;s.</p>
+              <p>Our name has changed over the years.  The original name of our church when it was incorporated in 1868 was First Lutheran Church and Society of Glendale, Minnesota.  Now, some refer to us as &#x201c;The Biscay Church&#x201d;, officially our name is &#x201c;St. John&#x2019;s Church&#x201d;.</p>
+            </div>
+            <div class="cleaner_with_height">&nbsp;</div>
+            
+            <div class="right_col_section">
+           	  <h1>Our Beliefs</h1>
+            	<p>
+              <ul>
+                <li>We believe the Bible, consisting of the Old and New Testaments, to be the only inspired, inerrant, infallible authoritative Word of God written.</li>
+                <li>We believe that there is one God, eternally existent in three persons: Father, Son, and Holy Spirit.</li>
+                <li>We believe in the deity of Christ, in His virgin birth, in His miracles, in His vicarious and atoning death through His shed blood, in His bodily resurrection, in His ascension to the right hand of the Father, and in His personal return in power and glory.</li>
+                <li>We believe that for salvation of lost and sinful man regeneration by the Holy Spirit is absolutely essential.</li>
+                <li>We believe in the present ministry of the Holy Spirit, by Whose indwelling power and fullness the Christian is enabled to live a godly life in the present evil world.</li>
+                <li>We believe in the resurrection of both the saved and the lost; they that are saved unto the resurrection of life and they that are lost unto the resurrection of damnation.</li>
+                <li>We believe in the spiritual unity of all believers in Christ.</li>
+</p>
+            </div>
+            <div class="cleaner">&nbsp;</div>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>St. John&rsquo;s Church &ndash; Biscay, MN</title>
+    <meta name="keywords" content="Christian, Church, Glencoe, Hutchinson, Minnesota, MN, Biscay, CCCC" />
+    <meta name="description" content="St. John's CCCC Church &ndash; Near Biscay, Minnesota" />
+    <link href="/style.css?v1.0" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="jquery-ui.min.css">
+    <link href="/style.css?v1.0" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="templatemo_container">
+      <div id="templatemo_header">
+        <img src="/images/header.jpg" usemap="#map" />
+        <map name="map">
+          <area shape="poly" coords="709, 297, 713, 258, 954, 257, 958, 350, 649, 352, 646, 323, 536, 321, 535, 285, 709, 291" href="http://www.ccccusa.com/" />
+        </map>
+      </div>
+      <div id="templatemo_menu">
+        <ul>
+          <li><a href="/"<?php if ($_SERVER['REQUEST_URI'] == "/"): ?> class="current" <?php endif; ?>>Home</a></li>
+          <li><a href="/downloads"<?php if ($_SERVER['REQUEST_URI'] == "/downloads/"): ?> class="current" <?php endif; ?>>Newsletters and Downloads</a></li>
+          <li><a href="https://www.youtube.com/channel/UCADuc53vyx21zzxjICn4NlQ">YouTube</a></li>
+          <li><a href="/about"<?php if ($_SERVER['REQUEST_URI'] == "/about"): ?> class="current" <?php endif; ?>>About St. John&rsquo;s</a></li>
+          <li><a href="/contact"<?php if ($_SERVER['REQUEST_URI'] == "/contact"): ?> class="current" <?php endif; ?> class="last">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div id="templatemo_content">
+
+        <div id="templatemo_left">
+          <div id="templatemo_news_section">
+            <h1>Happenings</h1>
+            <div class="scrollbar" style="max-height:225px;overflow-y:auto;">
+              @foreach($events as $event)
+              <div class="templatemo_news_box">
+                <h2>{{ $event->title }}</h2>
+                <h3>{{ date("F j", strtotime($event->date)) }}</h3>
+                <p>
+                  {{ $event->content }}
+                  @if($event->linktitle != "" && $event->linkcontent != "")
+                    <a href="{{ $event->linkcontent }}">{{ $event->linktitle }}</a>
+                  @endif
+                </p>
+              </div>
+              @endforeach
+            </div>
+          </div>
+          <div id="templatemo_contact_section">
+            <h1>Contact</h1>
+            <div class="templatemo_contact_box">
+              <ul class="info">
+                <li>
+                  <h3 class="font-awesome-h3"><i class="fa fa-phone"></i></h3>
+                  <p>Church: <a href="tel:320-587-5104">320-587-5104</a><br/>Pastor: <a href="tel:612-644-0628">612-644-0628</a></p>
+                </li>
+                <li>
+                  <h3 class="font-awesome-h3"><i class="fa fa-home"></i></h3>
+                  <p><a href="https://www.google.com/maps/place/13372+Nature+Ave,+Hutchinson,+MN+55350">13372 Nature Ave<br/>Hutchinson, MN 55350</a></p>
+                </li>
+                <li>
+                  <h3 class="font-awesome-h3"><i class="fa fa-envelope"></i></h3>
+                  <p>
+                    <a href="mailto:secretary@stjohnscccc.org">secretary@stjohnscccc.org</a>
+                    <br/>
+                    <a href="mailto:pastor@stjohnscccc.org">pastor@stjohnscccc.org</a>
+                  </p>
+                </li>
+                <li>
+                  <h3 class="font-awesome-h3"><i class="fa fa-video-camera"></i></h3>
+                  <p>
+                    <a href="https://www.youtube.com/channel/UCADuc53vyx21zzxjICn4NlQ">Check us out on YouTube:<br/>
+                      VBS, music, and more!</a>
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- note to self: use this! It's cool
+               <div class="left_col_section">
+               <a href="subpage.html"><img src="images/templatemo_ca.jpg" alt="Church Activities" border="0" /></a>
+               </div> -->
+        </div> <!-- end of left -->
+
+        <div id="templatemo_right">
+          @yield('content')
+        </div> <!-- end of right -->
+      </div> <!-- end of content -->
+
+      <div id="templatemo_footer">
+    	&copy; 2014-{{ date("Y") }} <a href="/">St. John's Church</a> &ndash; Site Design by <a href="https://chandlerswift.com">Chandler Swift</a> &ndash; <a href="admin/">Log In</a>
+      </div> <!-- end of footer -->
+    </div> <!-- end of container -->
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="/jquery-ui.min.js"></script>
+    <script>
+      $( "#accordion" ).accordion({
+        heightStyle: "content"
+      });
+      $('.embedded-map').click(function() {
+        $('.embedded-map iframe').css("pointer-events", "auto");
+      });
+    </script>
+  </body>
+</html>
