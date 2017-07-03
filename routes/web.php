48a214013c6588b7f1c268e93e8bacfccda20b79
Chandler Swift <chandler@chandlerswift.com>
2017-07-03 00:25:51 +0000

    Updated routes; added auth

--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,22 @@ Route::get('/contact', 'SiteController@contact');
 Route::post('/contact/send', 'SiteController@sendMessage');
 
 // Authentication Routes...
-$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-$this->post('login', 'Auth\LoginController@login');
-$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Password Reset Routes...
-$this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-$this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-$this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+// Password Change Routes...
+Route::get('password/change', 'Auth\ChangePasswordController@showChangeForm')->middleware('auth')->name('password.change');
+Route::post('password/change', 'Auth\ChangePasswordController@change')->middleware('auth');
+
+// Admin Routes
 Route::get('admin', 'AdminController@index')->middleware('auth');
-
 Route::resource('admin/events', 'EventsController');
-Route::get('admin/events/{id}/restore', 'EventsController@restore');
-
+Route::get('admin/events/{id}/restore', 'EventsController@restore')->middleware('auth');
 Route::resource('admin/groups', 'GroupsController');
