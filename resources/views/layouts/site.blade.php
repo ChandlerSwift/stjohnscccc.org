86c01b39d1fccee8b055819047130e18676adb21
swiftisaac <ijs@swiftgang.net>
2020-04-19 20:29:26 -0500

    Deleted Easter banner

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,9 +11,7 @@
   </head>
   <body>
     <div id="templatemo_container">
-    <div style="background-color:lemonchiffon; font-size:18px; line-height: 150%; padding: 20px; text-align: center; margin-bottom: 20px;">Due to the increased risk of illness from the coronavirus outbreak, St. John's has made the decision to cancel all services and activities starting March 15. Instead, online services will be streamed on YouTube at the usual times. <a href="https://youtube.com/channel/UCADuc53vyx21zzxjICn4NlQ">Click this link to access our YouTube channel</a>.</div>
-    <div style="background-color:yellow; font-size:24px; line-height: 150%; padding: 20px; text-align: center; margin-bottom: 20px;"><b>He is risen!</b> Watch our Easter service: <a href="https://www.youtube.com/playlist?list=PLeZJuZyIXR7egm4aQNkA-nInQZe5ScD58">Click this link to watch it on YouTube</a>.</div>
-      <div id="templatemo_header">
+    <div style="background-color:lemonchiffon; font-size:18px; line-height: 150%; padding: 20px; text-align: center; margin-bottom: 20px;">Due to the increased risk of illness from the coronavirus outbreak, St. John's has made the decision to cancel all services and activities starting March 15. Instead, Sunday services and Wednesday bible studies will be streamed on YouTube at the usual times. <a href="https://youtube.com/channel/UCADuc53vyx21zzxjICn4NlQ">Click this link to access our YouTube channel</a>.</div>
         <img src="/images/header.jpg" usemap="#map" />
         <map name="map">
           <area shape="poly" coords="709, 297, 713, 258, 954, 257, 958, 350, 649, 352, 646, 323, 536, 321, 535, 285, 709, 291" href="http://www.ccccusa.com/" />
