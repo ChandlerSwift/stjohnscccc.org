1d8e15e4bb5a220317d039b669a19fcf82123823
Chandler Swift <chandler@chandlerswift.com>
2020-08-01 01:25:08 -0500

    Trust all proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,9 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = [
-        '192.168.1.1'
-    ];
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
